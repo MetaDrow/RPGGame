dfed3d309eee8893e6752c6234b74344fdcb7c53
Meta <brinteraw@gmail.com>
2022-12-12 22:09:53 +0300

    init commit

--- Program.cs
+++ Program.cs
@@ -1,22 +1,2 @@
-﻿using System;
-
-
-
-namespace RPGGame
-{
-	internal class Program
-	{
-		static void Main (string[] args)
-		{
-
-			//start program 
-			int x = 5; int y = 5;
-			Character player = new Character();
-			player.CheckCharacter(x,y);
-
-
-
-			Console.ReadLine();
-		}
-	}
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
--- README.md
+++
@@ -1,4 +0,0 @@
-Simple RPG games in console.
-Implemented:
-1. Architecture building( as of 01.11.22 started)
-
